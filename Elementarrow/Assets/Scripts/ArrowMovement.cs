0fa9e9cca90bd713787903d55a10c976021cd645
FuryFurious <ricu05@yahoo.de>
2015-04-17 21:12:42 +0200

    Flying Arrows

---
+++ Elementarrow/Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMovement : MonoBehaviour {
+
+    public float speed = 12.0f;
+    public Vector2 direction;
+
+    private bool isFlying = true;
+    private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        _rigidBody = GetComponent<Rigidbody2D>();
+
+        _rigidBody.AddForce(direction * speed);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (isFlying)
+        {
+            float angle = Mathf.Atan2(_rigidBody.velocity.y, _rigidBody.velocity.x) * Mathf.Rad2Deg;
+            _rigidBody.rotation = angle;
+        }
+
+	}
+
+    void OnTriggerEnter2D()
+    {
+        isFlying = false;
+        _rigidBody.isKinematic = true;
+    }
+}
---
+++ Elementarrow/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed = 5.0f;
+    public float jumpSpeed = 12.0f;
+
+    public Object arrowPrefab;
+
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * movementSpeed, Input.GetAxis("Vertical") * jumpSpeed, 0) * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = -mainCamera.transform.position.z;
+
+            Vector3 fromTo = mainCamera.ScreenToWorldPoint(mousePos) - transform.position;
+            float rotation = Mathf.Atan2(fromTo.y, fromTo.x) * Mathf.Rad2Deg;
+
+            GameObject obj = (GameObject)Instantiate(arrowPrefab);
+            obj.transform.position = transform.position;
+            ArrowMovement arrow = obj.GetComponent<ArrowMovement>();
+            arrow.direction = new Vector2(fromTo.x, fromTo.y);
+  
+            //Debug.DrawRay(transform.position ,fromTo, Color.red, 1.0f);
+        }
+    }
+}
