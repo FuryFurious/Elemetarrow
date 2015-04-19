bdffeee46b8aca0dc157d9722b874529a69ad547
Hawkcutter <jarek.business@googlemail.com>
2015-04-19 17:05:31 +0200

    xcvbbn

---
+++ Elementarrow/Assets/Scripts/ToggleCanvas.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleCanvas : MonoBehaviour {
+
+	public Canvas canvas1;
+	public Canvas canvas2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void toggle(){
+		canvas1.enabled = !canvas1.enabled;
+		canvas2.enabled = !canvas2.enabled;
+
+	}
+}
